7af0ef6e883f13af73cdad6a8f34b5cef62e08e3
Phillip713 <Phillip713@users.noreply.github.com>
2025-10-02 13:13:52 +0200

    Added header styling + settings button

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import CarsScreen from './src/screens/CarsScreen';
@@ -8,21 +8,50 @@ import SettingsScreen from './src/screens/SettingsScreen';
 import AppPermissions from './src/screens/AppPermissionsScreen';
 import Customization from './src/screens/CustomizationScreen';
 import ProfileBilling from './src/screens/ProfileBillingScreen';
+import { TouchableOpacity, Text } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+
 
 const Stack = createStackNavigator();
+
+function MyStack() {
+  const navigation = useNavigation<any>();
+  
+  return (
+    <Stack.Navigator initialRouteName='home'
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerRight: () => (
+          <TouchableOpacity style={{ paddingRight: 20 }}
+            onPress={() => navigation.navigate('settings')}>
+            <Entypo name="cog" size={24} color="black" />
+          </TouchableOpacity>
+        ),
+      }}>
+      <Stack.Screen 
+        name='home' component={HomeScreen} 
+        options={{ headerTitle: "Home" }}
+        />
+      <Stack.Screen 
+        name='cars' component={CarsScreen}
+        options={{ headerTitle: "Find Cars" }}
+        />
+      <Stack.Screen name='settings' component={SettingsScreen}/>
+      <Stack.Screen name='permission' component={AppPermissions}/>
+      <Stack.Screen name='customization' component={Customization}/>
+      <Stack.Screen name='profilebilling' component={ProfileBilling}/>
+    </Stack.Navigator> 
+  );
+}
 
 export default function App() {
   return (
     <CarServiceContext.Provider value={new DummyCarService}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='home'>
-          <Stack.Screen name='home' component={HomeScreen}/>
-          <Stack.Screen name='cars' component={CarsScreen}/>
-          <Stack.Screen name='settings' component={SettingsScreen}/>
-          <Stack.Screen name='permission' component={AppPermissions}/>
-          <Stack.Screen name='customization' component={Customization}/>
-          <Stack.Screen name='profilebilling' component={ProfileBilling}/>
-        </Stack.Navigator> 
+        <MyStack />
       </NavigationContainer>
     </CarServiceContext.Provider>
   );
